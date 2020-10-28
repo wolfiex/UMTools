42c66333cdebb4e3ed121b7812c1065d4832e7da
Dan Ellis <daniel.ellis.research@gmail.com>
2020-10-28 22:39:06 +0000

    Update danrepo.sh

--- lib/danrepo.sh
+++ lib/danrepo.sh
@@ -1,7 +1,12 @@
-/usr/bin/expect <<EOD
-spawn ~fcm/bin/mosrs-setup-gpg-agent
+#!/usr/bin/expect -f
+set timeout -1
+spawn /bin/bash
+send "source /home/d04/fcm/bin/mosrs-setup-gpg-agent\n"
 expect "Met Office Science Repository Service password:"
 send "$1\n"
-expect eof
-EOD
+interact
+
+# place in bash_profie as bashrc loads each time a new shell is opened. 
+# original command uses . file  instead of source file - these are equivalent
+
 
