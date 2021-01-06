bdd5b9502b0caf94fe17f9bdf0d09e89e46f5853
Dan Ellis <daniel.ellis.research@gmail.com>
2021-01-06 19:59:24 +0000

    Update pathsNpermissions.py

--- lib/pathsNpermissions.py
+++ lib/pathsNpermissions.py
@@ -12,17 +12,43 @@ home = os.environ['HOME']
 files = glob.glob(home+'/roses/u-*/')
 print(home,len(files),files)
 modi = dict([[i,os.path.getmtime(i)] for i in files])
-print(modi)
 files = sorted(files,key=lambda x: -modi[x] )
 
 for i,key in enumerate(files):
-	try: tlog  = tuple(open(key+'/trac.log'))[3].split('||')[2]
-	except IOError: tlog = '--------------------'	
-	print (i,key.split('/')[-2],tlog)
+	print (i,key.split('/')[-2])
 
-selection = files[int(input('\nSelect run to transfer:\n'))]
+selection = files[int(input('\nSelect run to check Paths:\n'))]
 print(selection)
 
+#selection = '/home/d02/dellis/roses/u-ca218/'
+
+# get lines from grep
+lines = os.popen('grep -inr "[\\"\']\s*/[^\\"\']*\'" %s*'%selection).readlines()
+# get actual links 
+lines = [re.findall("[\"'](\s*/[^\"']*)",i)[0] for i in lines]
+# filter out links with 
+lines = set(filter(lambda x: re.match(r'/[^/]+/',x), lines))
 
 
-#grep -inr "[\"']\s*/[^\"']*" *
+''' 
+Having obtained all our paths we check if we have permissions to read them
+'''
+
+groups = os.popen('groups').read().split()
+groups.append(os.popen('echo $USER').read().strip())
+
+print 'Number of links: ' , len(lines)
+
+for i in lines:
+    fail = False    
+    if 'No such file' in os.popen('ls '+i):
+	fail = True
+    else:
+	permissions = os.popen('namei -l '+i).readlines()
+	selection = filter(lambda x: x[0]+='d',permissions)
+	print selection
+	
+
+
+
+
